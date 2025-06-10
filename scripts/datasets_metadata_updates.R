3e26c8a7ec6b80d3c0bdc7fc95d998b292aaf2aa
Cohen <avco10@charite.de>
2025-06-10 18:59:31 +0200

    extracted secondary id where it exists (didn't incorporate into pipline yet), fixed and created more plots, created 12 more nonmatched cases for metadata to replace "non matched" au_ov, changes OSF to DOI format, fixed zenodo id shift issue in charite and metadata file

--- scripts/datasets_metadata_updates.R
+++ scripts/datasets_metadata_updates.R
@@ -192,7 +192,25 @@ datasets_metadata_master_updated_003 <- datasets_metadata_master_updated_002 |>
 metadata_update(datasets_metadata_master_updated_003) # call function to save as csv, xlsx, rda
 
 
-# 004: add missing metadata from Evgeny -----------------------------------------------
+# 004: fix zenodo cases where data_id_merged is shifted -----------------------------------------------
+
+# load
+load_latest_metadata_update() # call function to load latest version
+
+datasets_metadata_master_updated_004 <- datasets_metadata_master_updated_003 |>
+  left_join(charite_dois_and_ids_8_for_matching |>
+              select(unique_id, data_id_m_val, data_id_merged),
+            by = "unique_id") |> 
+  select(-c(data_id_m_val.x, data_id_merged.x)) |> 
+  rename(data_id_m_val = data_id_m_val.y,
+         data_id_merged = data_id_merged.y) |> 
+  relocate(data_id_m_val, .after = data_id_no_ex_chr) |>
+  relocate(data_id_merged, .after = data_id_m_val)
+
+# save
+metadata_update(datasets_metadata_master_updated_004) # call function to save as csv, xlsx, rda
+
+# 00?: add missing metadata from Evgeny -----------------------------------------------
 
 # load
 load_latest_metadata_update() # call function to load latest version
