f1de3bcdd61c184075dea4e5ec1e282e54133644
Cohen <avco10@charite.de>
2025-08-22 17:07:47 +0200

    ran ds_primary again until m_val, included, in order to: make sure joins are correct using paired doi and id cols, always keep original doi and id inputs, name variables in a more explanatory way, add different ids of https://physionet.org/content/ptb-xl/1.0.1/. Next steps: prepare m_val for matching, match, update plots, archive older versions of plots inputs. Then continue to DataStet and Added.

--- scripts/manual_validation.R
+++ scripts/manual_validation.R
@@ -150,3 +150,114 @@ write_csv_cr(
 # And that's it.
 # From here, return to "charite_loading_and_preprocessing.qmd" to get the relevant cases from
 # "charite_dois_and_ids_6_m_val_done"
+
+
+# 4. Restrcutre _6_m_val_done ---------------------------------------------
+
+# 22.08.2025:
+#
+# I ran ds_primary qmd again in order to:
+#
+# 1. leave multiple charite dois in the table for later joins
+# 2. pair dois and ids into one column for later joins
+# 3. consider https://physionet.org/content/ptb-xl/1.0.1/ as multiple ids for the same dataset
+# 4. rename variables so that they will reflect their content more accurately (e.g. numbat_da instead of charite) 
+#
+# That's why here I'll restructure m_val according to the newest auto_cleaned output:
+#
+# numbat_da_dois_and_ids_3_auto_cleaned.RData
+#
+# Notice: the former m_val starts with charite. the new structure starts with numbat_da.
+# So you can archive all "charite_..." files.
+
+# load new structure auto cleaned
+load(here("data","wrangling_steps","charite","numbat_da_dois_and_ids_3_auto_cleaned.RData"))
+
+# load latest manual validation in the old structure
+load(here("data", "wrangling_steps", "charite", "charite_dois_and_ids_4_m_val_done.RData"))
+
+# colnames
+colnames(numbat_da_dois_and_ids_3_auto_cleaned)
+colnames(charite_dois_and_ids_4_m_val_done)
+
+# check for upper cases
+# doi
+any(grepl("[A-Z]", numbat_da_dois_and_ids_3_auto_cleaned$doi)) # new has, as expected
+any(grepl("[A-Z]", charite_dois_and_ids_4_m_val_done$doi)) # original doesn't have
+# id (lc)
+any(grepl("[A-Z]", numbat_da_dois_and_ids_3_auto_cleaned$data_id_lc)) # new doesn't have, as expected
+any(grepl("[A-Z]", charite_dois_and_ids_4_m_val_done$data_id)) # original also doesn't, as expected
+
+# So I'll pair (and then join) by doi_cl and 
+
+# add paired auto-cleaned for latest manual validation
+
+charite_dois_and_ids_4_m_val_done_for_joining_1 <- charite_dois_and_ids_4_m_val_done|>
+  mutate(doi_id_lc_pair_for_joining = paste(doi, data_id, sep = ";")) |> # pair lower case doi-id
+  distinct()
+
+numbat_da_dois_and_ids_3_auto_cleaned_for_joining <- numbat_da_dois_and_ids_3_auto_cleaned|>
+  mutate(doi_id_lc_pair_for_joining = paste(doi, data_id_lc, sep = ";")) |> # pair lower case doi-id
+  distinct()
+
+
+# prepare latest manual validation for joining
+
+colnames(charite_dois_and_ids_4_m_val_done_for_joining_1) # check which columns to keep
+
+charite_dois_and_ids_4_m_val_done_for_joining_2 <- charite_dois_and_ids_4_m_val_done_for_joining_1 |>
+  select(
+    doi_id_lc_pair_for_joining,
+    data_id_m_val,
+    validated,
+    Category,
+    is_gen_rep)
+  
+# join to new auto-cleaned
+
+numbat_da_dois_and_ids_4_m_val_joined <- numbat_da_dois_and_ids_3_auto_cleaned_for_joining |>
+  left_join(charite_dois_and_ids_4_m_val_done_for_joining_2,
+            by = "doi_id_lc_pair_for_joining") |>
+  relocate(data_id_m_val, .after = slug) |>
+  relocate(doi_id_orig_pair, .after = is_gen_rep) |>
+  relocate(doi_id_lc_pair_for_joining, .after = doi_id_orig_pair)
+  
+# check if there's anything else to add
+numbat_da_dois_and_ids_4_m_val_joined |> dplyr::filter(
+  is.na(validated) | is.na(Category) | is.na(is_gen_rep)) |> View()
+
+# Yes there is.
+
+# So I'll save numbat_da_dois_and_ids_4_m_val_joined as "in progress"
+# and then a csv for documentation and one to actually fill.
+
+# save
+save_cr(numbat_da_dois_and_ids_4_m_val_joined,
+        file = file.path(here("data", "wrangling_steps", "charite", "numbat_da_dois_and_ids_4_m_val_joined_in_progress.RData")))
+
+# Write a csv for documentation
+
+write_csv_cr(
+  numbat_da_dois_and_ids_4_m_val_joined,
+  file = here("data",
+              "verification",
+              "m_val",
+              "numbat_da_dois_and_ids_4_m_val_joined_in_progress.csv"),
+  row.names = FALSE
+)
+
+# Write another copy of it - this is the file you should work on!
+
+write_csv_cr(
+  numbat_da_dois_and_ids_4_m_val_joined,
+  file = here("data",
+              "verification",
+              "m_val",
+              "numbat_da_dois_and_ids_5_m_val_joined_done.csv"),
+  row.names = FALSE
+)
+
+# and that's it.
+# From here, return to "charite_loading_and_preprocessing.qmd" to get the relevant cases from
+# "numbat_da_dois_and_ids_5_m_val_joined_done.csv"
+# it's saved there as an RData object as well ("numbat_da_dois_and_ids_5_m_val_joined_done.RData")
